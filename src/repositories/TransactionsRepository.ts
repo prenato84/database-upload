c260121d852e3bf1951fa3181a17c7b6102dfd0a
Paulo Castro <prenato84@gmail.com>
2020-07-02 18:30:00 -0300

    Features Create and List Transactions with Category

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,8 +1,8 @@
 import { EntityRepository, Repository } from 'typeorm';
 
-import csvParse from 'csv-parse';
-import fs from 'fs';
-import path from 'path';
+// import csvParse from 'csv-parse';
+// import fs from 'fs';
+// import path from 'path';
 
 import Transaction from '../models/Transaction';
 
@@ -15,34 +15,52 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += Number(transaction.value);
+            break;
+          case 'outcome':
+            accumulator.outcome += Number(transaction.value);
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
-
-  public async loadCSV(fileName: string): any[] {
-    const csvFilePath = path.resolve(__dirname, '..', '..', 'tmp', fileName);
-
-    const readCSVStream = fs.createReadStream(csvFilePath);
-
-    const parseStream = csvParse({
-      from_line: 2,
-      ltrim: true,
-      rtrim: true,
-    });
-
-    const parseCSV = readCSVStream.pipe(parseStream);
-
-    const lines = [];
-
-    parseCSV.on('data', line => {
-      lines.push(line);
-    });
-
-    await new Promise(resolve => {
-      parseCSV.on('end', resolve);
-    });
-
-    return lines;
-  }
+  // public async loadCSV(fileName: string): any[] {
+  //   const csvFilePath = path.resolve(__dirname, '..', '..', 'tmp', fileName);
+  //   const readCSVStream = fs.createReadStream(csvFilePath);
+  //   const parseStream = csvParse({
+  //     from_line: 2,
+  //     ltrim: true,
+  //     rtrim: true,
+  //   });
+  //   const parseCSV = readCSVStream.pipe(parseStream);
+  //   const lines = [];
+  //   parseCSV.on('data', line => {
+  //     lines.push(line);
+  //   });
+  //   await new Promise(resolve => {
+  //     parseCSV.on('end', resolve);
+  //   });
+  //   return lines;
+  // }
 }
 
 export default TransactionsRepository;
